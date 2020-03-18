bf80fae11250d6525e4e060b402d1a6a46184fc1
guanwill <w.guan@hotmail.com>
2020-03-18 20:01:44 +1100

    update

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 
-mongoose.connect('mongodb://localhost/mgl', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/mgl', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>  console.log('connection successful'))
   .catch((err) => console.error(err));
